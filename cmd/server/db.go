1ab3460bf7446e449f8dd4735017a587da4fce2a
Alexandre Saison <alexandre.saison@inarix.com>
2026-01-06 10:28:34 +0100

    (db): using both gorm + prom plugin

--- cmd/server/db.go
+++ cmd/server/db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/uptrace/opentelemetry-go-extra/otelgorm"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/plugin/prometheus"
@@ -16,16 +17,20 @@ func initDatabase(connectionString string) (*gorm.DB, error) {
 	if errOpenDial != nil {
 		return nil, errOpenDial
 	}
-	errPromConnect := dbConn.Use(prometheus.New(
+	promPlug := prometheus.New(
 		prometheus.Config{
 			DBName:           os.Getenv("POSTGRES_DB"), // use `DBName` as metrics label
 			RefreshInterval:  15,                       // Refresh metrics interval (default 15 seconds)
 			StartServer:      true,                     // start http server to expose metrics
 			HTTPServerPort:   8080,                     // configure http server port, default port 8080 (if you have configured multiple instances, only the first `HTTPServerPort` will be used to start server)
 			MetricsCollector: []prometheus.MetricsCollector{&prometheus.Postgres{Interval: 15}},
-		}))
-	if errPromConnect != nil {
+		})
+
+	if errPromConnect := dbConn.Use(promPlug); errPromConnect != nil {
 		return nil, errPromConnect
+	}
+	if errOtel := dbConn.Use(otelgorm.NewPlugin(otelgorm.WithDBName("munchin"))); errOtel != nil {
+		return nil, errOtel
 	}
 
 	return dbConn, nil
