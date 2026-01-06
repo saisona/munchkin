1b06c70b21ab643b1aae5d80b2bb5398a8185756
Alexandre Saison <alexandre.saison@inarix.com>
2026-01-06 10:30:15 +0100

    (pkg:log): logger for otel + loki

---
+++ pkg/log/otel_handler.go
@@ -0,0 +1,44 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+type OTelHandler struct {
+	next slog.Handler
+}
+
+func NewOTelHandler(next slog.Handler) slog.Handler {
+	return &OTelHandler{next: next}
+}
+
+func (h *OTelHandler) Enabled(ctx context.Context, lvl slog.Level) bool {
+	return h.next.Enabled(ctx, lvl)
+}
+
+func (h *OTelHandler) Handle(ctx context.Context, r slog.Record) error {
+	span := trace.SpanFromContext(ctx)
+	if span != nil {
+		sc := span.SpanContext()
+		fmt.Println("sc=>", sc)
+		if sc.IsValid() {
+			r.AddAttrs(
+				slog.String("trace_id", sc.TraceID().String()),
+				slog.String("span_id", sc.SpanID().String()),
+			)
+		}
+	}
+	return h.next.Handle(ctx, r)
+}
+
+func (h *OTelHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &OTelHandler{next: h.next.WithAttrs(attrs)}
+}
+
+func (h *OTelHandler) WithGroup(name string) slog.Handler {
+	return &OTelHandler{next: h.next.WithGroup(name)}
+}
